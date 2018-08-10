cf710636350de178083c698248b1944f766c005f
adtimokhin <adtimokhin@gmail.com>
2018-08-10 14:43:29 +0300

    Initial commit

---
+++ android/src/ru/adtimokhin/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.adtimokhin;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.adtimokhin.StarShooterGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarShooterGame(), config);
+	}
+}
